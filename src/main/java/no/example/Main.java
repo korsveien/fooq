b3da9782d6fe83e49f525eb80f72506d1b5c4694
Peder Korsveien <peder.korsveien@gmail.com>
2017-05-25 13:00:21 +0200

    Use jackson

--- src/main/java/no/example/Main.java
+++ src/main/java/no/example/Main.java
@@ -1,8 +1,11 @@
 package no.example;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.vavr.collection.List;
+import io.vavr.jackson.datatype.VavrModule;
+import lombok.SneakyThrows;
 
 import java.time.OffsetDateTime;
 
@@ -10,11 +13,18 @@ import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        TransactionsDTO transactionsDTO = TransactionsDTO.of(OffsetDateTime.now(), List.of("sdsd", "Sdsd"));
+        TransactionsDTO transactionsDTO = TransactionsDTO.of(OffsetDateTime.now(), List.of("sdsd", "dsds"));
+        get("/transactions", (req, res) -> toJson(transactionsDTO));
+    }
 
-        Gson gson = new GsonBuilder().registerTypeAdapter(OffsetDateTime.class, new OffsetDateTimeAdapter()).create();
-
-        get("/transactions", (req, res) -> gson.toJson(transactionsDTO));
+    @SneakyThrows
+    private static String toJson(Object data) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new VavrModule());
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(data);
     }
 
 }
